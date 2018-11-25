468bdde49613314c41bb8eb51e8f0018dd41dd94
Dale fernandes <fernandes.dale937@gmail.com>
2018-11-25 15:30:38 +0530

    Test not working.otherwise working

--- Code/nnet/optimizer.py
+++ Code/nnet/optimizer.py
@@ -11,9 +11,9 @@ def mbgd(weights, biases, dw1, db1, dw2, db2, dw3, db3, lr):
     weights['w1'] = weights['w1'] -(lr*dw1)
     weights['w2'] = weights['w2'] -(lr*dw2)
     weights['w3'] = weights['w3'] -(lr*dw3)
-    biases['b1'] = weights['b1'] -(lr*db1)
-    biases['b2'] = weights['b2'] -(lr*db2)
-    biases['b3'] = weights['b3'] -(lr*db3)
+    biases['b1'] = biases['b1'] -(lr*db1)
+    biases['b2'] = biases['b2'] -(lr*db2)
+    biases['b3'] = biases['b3'] -(lr*db3)
 
     return weights, biases
 
